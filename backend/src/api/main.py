75a7a5c614ff9e300e27c9d16879281b79748e94
mitchell_kimbell <mfkimbell@gmail.com>
2024-03-14 20:23:46 -0500

    stuck on this mongo test route

--- backend/src/api/main.py
+++ backend/src/api/main.py
@@ -1,16 +1,32 @@
 # STL
 import re
+from typing import Any, Dict
 
 # PDM
 import requests
 from fastapi import FastAPI, Request, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import Body
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
+from motor.motor_asyncio import AsyncIOMotorClient
 
 # initialization
 load_dotenv()
+
+MONGODB_URL = "mongodb+srv://mfkimbell:<password>@cluster0.0mzfhaf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DATABASE_NAME = "buildwise_database"
+COLLECTION_NAME = "users"
+
+mongo_client = AsyncIOMotorClient(MONGODB_URL)
+db = mongo_client[DATABASE_NAME]
+collection = db[COLLECTION_NAME]
+
+document = collection.find_one()
+print({"message": "Connected to MongoDB!", "data": document})
+
+
 api_key = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=api_key)
 app = FastAPI()
@@ -30,6 +46,20 @@ async def read_root():
     return {"Hello": "World"}
 
 
+@app.get("/mongo/test")
+async def root():
+    document = await collection.find_one()
+    return {"message": "Connected to MongoDB!", "data": document}
+
+
+@app.post("/mongo/{user_id}")
+async def post_user_graph(user_id: str, graph_data: Dict[str, Any] = Body(...)):
+    # Assume graph_data is already in the desired JSON-like dict format
+    graph_data["userID"] = user_id  # Add the user_id to the graph data
+    await collection.insert_one(graph_data)
+    return {"message": "Graph saved successfully."}
+
+
 @app.post("/api/testgpt")
 async def test_gpt(request: Request):
     request_json = await request.json()  # Parse JSON body
@@ -37,7 +67,8 @@ async def test_gpt(request: Request):
     adv_user_input = (
         user_input + " Additionally, "
         "These results are intended to create a visual map for project architecture so the user can better understand how the project interacts. "
-        "The format of the results of this query should be json object containing two lists as its result. Here is an example of how I want the results of this query to be: "
+        "The format of the results of this query should be json object containing two lists as its result. Here is an example of how I want the results of this query to be: \
+        "
         '{"initialNodes": [{"id": "react","type": "custom","data": { "label": "React", "imgKey": "react" },"position": { "x": 250, "y": 5 },"isConnectable": true},'
         '{"id": "mongodb","type": "custom","data": { "label": "MongoDB", "imgKey": "mongodb" },"position": { "x": 100, "y": 100 },"isConnectable": true},'
         '{"id": "docker","type": "custom","data": { "label": "Docker", "imgKey": "docker" },"position": { "x": 400, "y": 100 },"isConnectable": true}],'
